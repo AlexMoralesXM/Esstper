bc44c4ab4d903e27c0ab186d4c38e3b173fc8b37
Oprea Alexandru <pasotee@users.noreply.github.com>
2017-11-30 23:26:33 +0200

    Map layout for tutorial

---
+++ Source/Esstper/TutorialGameMode.h
@@ -0,0 +1,20 @@
+// E
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "TutorialGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESSTPER_API ATutorialGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};

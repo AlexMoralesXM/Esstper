37a5af8be775abb8b735ea3138973318692a08b7
Oprea Alexandru <pasotee@users.noreply.github.com>
2017-11-30 10:42:21 +0200

    Modules added and HUD_BP for intro game mode

--- Source/Esstper/IntroHUD.cpp
+++ Source/Esstper/IntroHUD.cpp
@@ -1,7 +1,23 @@
 // pasotee 2017
 
 #include "IntroHUD.h"
+#include "Widgets/SUserWidget.h"
+#include "Blueprint/UserWidget.h"
 
+void AIntroHUD::BeginPlay()
+{
+	Super::BeginPlay();
 
+	if (StoryWidgetTemplate == nullptr)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Missing StoryWidgetTemplate on intro HUD."));
+		return;
+	}
+	else
+	{
+		StoryWidget = CreateWidget<UUserWidget>(GetWorld(), StoryWidgetTemplate);
+		StoryWidget->AddToViewport();
+	}
+}
 
 
--- Source/Esstper/IntroHUD.h
+++ Source/Esstper/IntroHUD.h
@@ -7,14 +7,25 @@
 #include "IntroHUD.generated.h"
 
 /**
- * 
+ * Updates and changes the Intro HUD
  */
+
+class UUserWidget;
+
 UCLASS()
 class ESSTPER_API AIntroHUD : public AHUD
 {
 	GENERATED_BODY()
-	
-	
-	
-	
+
+protected:
+	// Widget used to story telling.
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Story")
+	TSubclassOf<UUserWidget> StoryWidgetTemplate;
+
+private:
+	// Widget Instance for the story widget template.
+	UUserWidget* StoryWidget;
+
+protected:
+	void BeginPlay() override;
 };
